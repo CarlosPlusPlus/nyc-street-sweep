55c965efe04ba66209de5fa6fea818b638170e98
cjlwired <carlos.jose.lazo@gmail.com>
2013-06-25 13:50:32 -0400

    Update app.rb and form to take in form updates.

--- app.rb
+++ app.rb
@@ -7,8 +7,39 @@ module NycStreetSweep
 
     # View setup for base index page.
     get '/' do
+      
+      @main = "Default - Main St"
+      @from = "Default - From St"
+      @to   = "Default - To St"
+      @side = "Default - Side St"
+
+      @regulation_str = "Your regulation will go HERE"
+      
       erb :form_test
     end
-  
+
+    post '/' do
+
+      @main = params[:main_st]
+      @from = params[:from_st]
+      @to   = params[:to_st]
+      @side = params[:side_st]
+      
+      ######################
+      # OUR CODE GOES HERE #
+      ######################
+
+      # Geocoder -> Main St + Cross St 1
+      #          -> Main St + Cross St 2
+
+      #       Returns Array of [Lat,Long]
+
+      # Call Geocoder.rb to generate (x1,y1) to (x2,y2) lat/long.
+
+      @regulation_str = "POSTED SOURCE"
+
+      erb :form_test
+    end
+
   end
 end
