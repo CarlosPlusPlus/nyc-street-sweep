bfa52ffa7ca29d05c97e75a788dd1271277cefb3
cjlwired <carlos.jose.lazo@gmail.com>
2013-06-26 11:56:52 -0400

    Add text on submit functionality.

--- app.rb
+++ app.rb
@@ -19,15 +19,19 @@ module NycStreetSweep
 
     # Route for form submission.
     post '/' do
-      @main = params[:main_st]
-      @from = params[:from_st]
-      @to   = params[:to_st]
-      @side = params[:side_st]
+      @main  = params[:main_st]
+      @from  = params[:from_st]
+      @to    = params[:to_st]
+      @side  = params[:side_st]
+      @text  = params[:text]
+      @tweet = params[:tweet]
     
       # Determine regulation based on template values.
-      parse_values = Parser.run_parsing(@main,@from,@to,@side)
-
+      parse_values    = Parser.run_parsing(@main,@from,@to,@side)
       @regulation_str = "Street cleaning takes place between #{parse_values[0][0].strftime("%k:%M%p")} and #{parse_values[0][1].strftime("%k:%M%p")}\non #{parse_values[1]}"
+
+      # Text and Tweet regulation info.
+      Text.send(@text,@regulation_str)
 
       erb :form_test
     end
