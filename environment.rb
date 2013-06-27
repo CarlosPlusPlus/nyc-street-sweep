b0aa085e859a2b230824bf4c088ccb8053659015
Matthew Schmaus <mschmaus201@gmail.com>
2013-06-27 12:10:39 -0400

    Updated map sizing and fixed env reqs

--- environment.rb
+++ environment.rb
@@ -1,7 +1,13 @@
+require_relative 'lib/models/mapgenerator.rb'
+require_relative 'lib/models/parser.rb'
+require_relative 'lib/models/scheduler.rb'
+require_relative 'lib/models/text.rb'
+require_relative 'lib/models/tweet.rb'
+
 # Load all ruby files in /lib directory.
 
-Dir.glob('/lib').each do |folder|
-  Dir.glob(folder +"/*.rb").each do |file|
-    require file
-  end
-end
+# Dir.glob('/lib').each do |folder|
+#   Dir.glob(folder +"/*.rb").each do |file|
+#     require file
+#   end
+# end
